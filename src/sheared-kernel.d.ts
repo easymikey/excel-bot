c2058bffc584bf81930e72afcc1a20a72c74b5be
Mike Avdeev <avdeev2033@icloud.com>
2021-11-01 14:50:21 +0300

    feat: update sheared-kernel.d.ts

--- src/sheared-kernel.d.ts
+++ src/sheared-kernel.d.ts
@@ -1,12 +1,19 @@
 type TDate = string;
 
-type TRequest = {
+type TDownloadRequest = {
   name: string;
   created: TDate;
   modified: TDate;
-  mime_type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+  mime_type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
   file: string;
-  media_type: "document";
+  media_type: 'document';
   path: string;
-  type: "file";
+  type: 'file';
+};
+
+type TUploadRequest = { href: string; method: string };
+
+type ApiResponse<T> = {
+  data: T;
+  status?: number;
 };
